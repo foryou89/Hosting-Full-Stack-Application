e6e3f3fec66459a7b3e5e221531f9dd08ccde894
Ahmed Mohamed <forex.terad@gmail.com>
2022-11-05 01:52:14 +0200

    fix front-end lint

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: true,
-  appName: "Udagram",
-  apiHost: "http://udagramapp-env.eba-wpbcph6u.us-east-1.elasticbeanstalk.com/api/v0",
+  appName: 'Udagram',
+  apiHost: 'http://udagramapp-env.eba-wpbcph6u.us-east-1.elasticbeanstalk.com/api/v0',
 };
 
 /*
