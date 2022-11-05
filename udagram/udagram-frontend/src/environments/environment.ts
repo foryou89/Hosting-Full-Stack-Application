6dc7c637e242aa28ab9af87097fbb7a852d6a638
Ahmed Mohamed <forex.terad@gmail.com>
2022-11-05 03:10:23 +0200

    change api host url

--- udagram/udagram-frontend/src/environments/environment.ts
+++ udagram/udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://udagramapp-env.eba-wpbcph6u.us-east-1.elasticbeanstalk.com/api/v0'
+  apiHost: 'http://udagram-app-env.eba-jc6hxwnp.us-east-1.elasticbeanstalk.com/api/v0'
 };
 
 /*
